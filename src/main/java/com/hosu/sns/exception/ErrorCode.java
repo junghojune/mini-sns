71e63bef42bf966ebca210da6332519910f7083e
hosu <hojune0904@gmail.com>
2023-02-02 15:03:20 +0900

    add exception code about login

--- src/main/java/com/hosu/sns/exception/ErrorCode.java
+++ src/main/java/com/hosu/sns/exception/ErrorCode.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated"),
+    USER_NOT_FOUNR(HttpStatus.NOT_FOUND, "User not found"),
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Password is invalid"),
 
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "internal server error"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     ;
 
     private HttpStatus status;
