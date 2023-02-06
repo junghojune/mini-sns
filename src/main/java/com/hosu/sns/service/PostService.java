b1458b722ef2b64ff506cfd28a9b3384c5bb0424
hosu <hojune0904@gmail.com>
2023-02-06 17:08:13 +0900

    post 삭제 테스트 코드

--- src/main/java/com/hosu/sns/service/PostService.java
+++ src/main/java/com/hosu/sns/service/PostService.java
@@ -48,4 +48,20 @@ public class PostService {
 
         return Post.fromEntity(postEntityRepository.saveAndFlush(postEntity));
     }
+
+    @Transactional
+    public void delete(String userName, Integer postId){
+        // user find
+        UserEntity userEntity = userEntityRepository.findByUserName(userName)
+                .orElseThrow(() -> new SnsApplicationException(USER_NOT_FOUND, String.format("%S not founded", userName)));
+        // post exist
+        PostEntity postEntity = postEntityRepository.findById(postId)
+                .orElseThrow(() -> new SnsApplicationException(POST_NOT_FOUND, String.format("%s not founded", postId)));
+        // post permission
+        if (postEntity.getUser() != userEntity) {
+            throw new SnsApplicationException(INVALID_PERMISSION, String.format("%s has no permission with %s", userName, postId));
+        }
+
+        postEntityRepository.delete(postEntity);
+    }
 }
--- src/test/java/com/hosu/sns/controller/PostControllerTest.java
+++ src/test/java/com/hosu/sns/controller/PostControllerTest.java
@@ -18,12 +18,13 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static com.hosu.sns.exception.ErrorCode.INVALID_PERMISSION;
+import static com.hosu.sns.exception.ErrorCode.POST_NOT_FOUND;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -118,12 +119,53 @@ public class PostControllerTest {
         String title = "title";
         String body = "body";
 
-        //TODO mocking
-        doThrow(new SnsApplicationException(ErrorCode.POST_NOT_FOUND))
+        doThrow(new SnsApplicationException(POST_NOT_FOUND))
                 .when(postService).modify(eq(title), eq(body), any(), eq(1));
         mockMvc.perform(put("api/v1/posts/1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(new PostModifyRequest(title, body)))
+                ).andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트삭제() throws Exception {
+        mockMvc.perform(delete("api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void 포스트삭제시_로그인하지_않은경우() throws Exception {
+        mockMvc.perform(delete("api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트삭제시_작성자와_요청자가_다를경우() throws Exception {
+        //mocking
+        doThrow(new SnsApplicationException(INVALID_PERMISSION)).when(postService).delete(any(), any());
+
+        mockMvc.perform(delete("api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void 포스트삭제시_삭제하려는_포스트가_존재하지_않을경우() throws Exception {
+        //mocking
+        doThrow(new SnsApplicationException(POST_NOT_FOUND)).when(postService).delete(any(), any());
+
+        mockMvc.perform(delete("api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
                 ).andDo(print())
                 .andExpect(status().isNotFound());
     }
