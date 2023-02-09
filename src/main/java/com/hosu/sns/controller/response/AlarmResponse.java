dd834f67c22ae39755ae5c3ef6b51f9f2a8201c4
hosu <hojune0904@gmail.com>
2023-02-09 16:16:53 +0900

    알람 저장 로직

--- src/main/java/com/hosu/sns/controller/response/AlarmResponse.java
+++ src/main/java/com/hosu/sns/controller/response/AlarmResponse.java
@@ -3,14 +3,12 @@ package com.hosu.sns.controller.response;
 import com.hosu.sns.model.Alarm;
 import com.hosu.sns.model.AlarmArgs;
 import com.hosu.sns.model.AlarmType;
-import com.hosu.sns.model.User;
-import com.hosu.sns.model.entity.AlarmEntity;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 
 import java.sql.Timestamp;
 
-@Getter
+@Data
 @AllArgsConstructor
 public class AlarmResponse {
     private Integer id;
@@ -21,7 +19,7 @@ public class AlarmResponse {
     private Timestamp updatedAt;
     private Timestamp deletedAt;
 
-    public static AlarmResponse fromAlarm(Alarm alarm){
+    public static AlarmResponse fromAlarm(Alarm alarm) {
         return new AlarmResponse(
                 alarm.getId(),
                 alarm.getAlarmType(),
--- src/main/java/com/hosu/sns/model/AlarmArgs.java
+++ src/main/java/com/hosu/sns/model/AlarmArgs.java
@@ -1,5 +1,6 @@
 package com.hosu.sns.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
@@ -7,6 +8,7 @@ import lombok.Data;
  * 좋지 않다. 그래서 args를 성정하여 좀 더 유연하게 데이터를 처리할 수 있다.
  */
 @Data
+@AllArgsConstructor
 public class AlarmArgs {
 
     // 알람을 발생시킨 유저아이디
